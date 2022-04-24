2a226101a6ba4e63820c3542ec465fa9205fdc18
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-04-24 19:25:29 -0400

    create a show page and create route for this

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/show', { campground })
+})
+
+
 app.listen(3000, () => {
   console.log("Listening on PORT 3000")
 })
