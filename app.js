ee161fb91f06437e0a7f0399937a5486a5f7784f
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-04-24 19:50:24 -0400

    create a new add form, create  routes accordingly

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const path = require('path')
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
-
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -31,11 +31,22 @@ app.get('/campgrounds', async (req, res) => {
   res.render('campgrounds/index', { campgrounds })
 })
 
+app.get('/campgrounds/new', (req, res) => {
+  res.render('campgrounds/new')
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground)
+  await campground.save()
+  res.redirect(`campgrounds/${campground._id}`)
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   res.render('campgrounds/show', { campground })
 })
+
 
 
 app.listen(3000, () => {
