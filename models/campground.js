301043c8d44c85982e2a399d224f4c1ac7425f7b
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-04-24 18:14:47 -0400

    request all packages, connect db mongo

---
+++ models/campground.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+
+const CampgroundSchema = new Schema({
+  title: String,
+  price: String,
+  description: String,
+  location: String
+})
+
+
+
+
+module.exports = mongoose.model('Campground', CampgroundSchema)
