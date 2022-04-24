c71c178dce488d5d6939d3a914cdfb385af46630
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-04-24 19:02:13 -0400

    display all camps

--- app.js
+++ app.js
@@ -26,10 +26,9 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
-app.get('/campground', async (req, res) => {
-  const camp = new Campground({title: 'My Backyard', description: 'cheap camp' })
-  await camp.save()
-  res.send(camp)
+app.get('/campgrounds', async (req, res) => {
+  const campgrounds = await Campground.find({})
+  res.render('campgrounds/index', { campgrounds })
 })
 
 app.listen(3000, () => {
