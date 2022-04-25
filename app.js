a8914c8a57eb6a794bdab924d42e1d94411b2849
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-04-24 20:13:16 -0400

    add edit route and create a file for this

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const methodOverride = require('method-override');
+
 const Campground = require('./models/campground')
+
 mongoose.connect('mongodb://localhost:27017/yelp-camp'), {
   useNewUrlParser: true,
   useCreateIndex: true,
@@ -20,7 +23,8 @@ const path = require('path')
 
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -48,6 +52,17 @@ app.get('/campgrounds/:id', async (req, res) => {
 })
 
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render('campgrounds/edit', { campground })
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+  res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.listen(3000, () => {
   console.log("Listening on PORT 3000")
