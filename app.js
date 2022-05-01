a6189a21bdeed2cfa19b7adb4779bc8d3cc8678e
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-05-01 18:21:38 -0400

    install joi validation for data

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const methodOverride = require('method-override');
 //layout extension instead of partials
 const ejsMate = require('ejs-mate');
+const Joi = require('joi')
 const ExpressError = require("./utils/ExpressError")
 const catchAsync = require("./utils/catchAsync")
 
@@ -48,7 +49,23 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
-  if (!req.body.campground) throw new ExpressError('Invalid campground Data', 400)
+  // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)
+  const campgroundSchema = Joi.object({
+    campground: Joi.object({
+      title: Joi.string().required(),
+      price: Joi.number().required().min(0),
+      image: Joi.string().required(),
+      location: Joi.string().required(),
+      description: Joi.string().required()
+    }).required()
+  })
+  const { error } = campgroundSchema.validate(req.body)
+  console.log("ERROR", error)
+
+  if (error) {
+    const msg = error.details.map(el => el.message).join(",")
+    throw new ExpressError(msg, 400)
+  }
   const campground = new Campground(req.body.campground)
   await campground.save()
   res.redirect(`campgrounds/${campground._id}`)
@@ -84,11 +101,11 @@ app.all("*", (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-  const { statusCode = 500, message = 'Something went wrong' } = err;
-  res.status(statusCode).send(message)
-  res.send('Something went wrong')
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Something went wrong"
+  res.status(statusCode).render('error', { err })
 })
 
 app.listen(3000, () => {
   console.log("Listening on PORT 3000")
-})
+}) 
