e7419b4bc8c2e7e4846e677a72ed66aa05ee285f
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-05-04 21:57:03 -0400

    implement query middleware for campground

--- models/campground.js
+++ models/campground.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
-
+const Review = require('./review')
 
 const CampgroundSchema = new Schema({
   title: String,
@@ -16,7 +16,15 @@ const CampgroundSchema = new Schema({
   ]
 })
 
-
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+  if(doc){
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews
+      }
+    })
+  }
+})
 
 
 module.exports = mongoose.model('Campground', CampgroundSchema)
