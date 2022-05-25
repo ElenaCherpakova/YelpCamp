63c749a913abab3480aad20bebf23cf2da981ea5
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-05-24 21:23:45 -0400

    remove not used file in users.controller

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router();
-const User = require('../models/user')
 const catchAsync = require("../utils/catchAsync")
 const passport = require('passport');
 const users = require("../controllers/users")
