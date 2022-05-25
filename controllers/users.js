da843e9ab580160f16a8e25197196141490db773
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-05-24 21:03:16 -0400

    create user in controller and transfer all logic there

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const User = require('../models/user')
+
+module.exports.renderRegister = (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.redirect('/campgrounds');
+  }
+  res.render('users/register')
+}
+
+module.exports.register = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body
+    const user = new User({ username, email })
+    const registeredUser = await User.register(user, password)
+    req.login(registeredUser, err => { //no await here because it doesn't support it
+      if (err) return next(err)
+      req.flash('success', `Welcome to Yelp Camp, ${username}!`)
+      res.redirect('/campgrounds')
+    })
+
+  } catch (err) {
+    req.flash('error', err.message)
+    res.redirect('/register')
+  }
+}
+
+module.exports.renderLogin = (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.redirect('/campgrounds');
+  }
+  res.render('users/login')
+}
+
+module.exports.login = (req, res) => {
+  const { username } = req.body
+  req.flash('success', `Welcome Back, ${username}!`)
+  const redirectUrl = req.session.returnTo || "/campgrounds";
+  delete req.session.returnTo;
+  res.redirect(redirectUrl)
+}
+module.exports.logout = (req, res) => {
+  req.logout()
+  req.flash('success', "See you Soon!")
+  res.redirect('/campgrounds')
+}
--- routes/users.js
+++ routes/users.js
@@ -4,55 +4,14 @@ const User = require('../models/user')
 const catchAsync = require("../utils/catchAsync")
 const passport = require('passport');
 const { resetWatchers } = require('nodemon/lib/monitor/watch');
-
-router.get('/register', (req, res) => {
-  if (req.isAuthenticated()) {
-    return res.redirect('/campgrounds');
-  }
-  res.render('users/register')
-})
+const users = require("../controllers/users")
 
 
-router.post('/register', catchAsync(async (req, res, next) => {
-  try {
-    const { username, email, password } = req.body
-    const user = new User({ username, email })
-    const registeredUser = await User.register(user, password)
-    req.login(registeredUser, err => { //no await here because it doesn't support it
-      if (err) return next(err)
-      req.flash('success', `Welcome to Yelp Camp, ${username}!`)
-      res.redirect('/campgrounds')
-    })
-
-  } catch (err) {
-    req.flash('error', err.message)
-    res.redirect('/register')
-  }
-}))
-
-
-
-router.get('/login', (req, res) => {
-  if (req.isAuthenticated()) {
-    return res.redirect('/campgrounds');
-  }
-  res.render('users/login')
-})
-
-router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
-  const { username } = req.body
-  req.flash('success', `Welcome Back, ${username}!`)
-  const redirectUrl = req.session.returnTo || "/campgrounds";
-  delete req.session.returnTo;
-  res.redirect(redirectUrl)
-
-})
-
-router.get('/logout', (req, res) => {
-  req.logout()
-  req.flash('success', "See you Soon!")
-  res.redirect('/campgrounds')
-})
+router.get('/register', users.renderRegister)
+router.post('/register', catchAsync(users.register))
+router.get('/login', users.renderLogin)
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
+router.get('/logout', users.logout)
 
 
 module.exports = router;
