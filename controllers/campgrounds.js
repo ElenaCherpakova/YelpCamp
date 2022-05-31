16a4d6430e18b633b1833a7aea8ce3a51eb6bfae
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-05-30 23:24:11 -0400

    install mapbox, add geometry and coordinates into campground model, geocode info from the form that user fills out and get data

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,6 +1,8 @@
 const Campground = require('../models/campground')
 const { cloudinary } = require("../cloudinary");
-
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 module.exports.index = async (req, res) => {
   const campgrounds = await Campground.find({})
   res.render('campgrounds/index', { campgrounds })
@@ -11,12 +13,17 @@ module.exports.renderNewForm = (req, res) => {
 }
 
 module.exports.createCampground = async (req, res, next) => {
+  const geoData = await geocoder.forwardGeocode({
+    query: req.body.campground.location,
+    limit: 1
+  }).send()
   const campground = new Campground(req.body.campground);
+  campground.geometry = geoData.body.features[0].geometry;
   //map array and store path and filename as an obj in req.files
   campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
-  console.log(campground.images)
   campground.author = req.user._id
   await campground.save();
+  console.log(campground)
   req.flash('success', "Successfully create campground")
   res.redirect(`campgrounds/${campground._id}`)
 }
