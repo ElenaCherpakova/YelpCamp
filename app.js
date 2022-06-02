267dcef0bc49501b520b9fa73e9529cd9dcbf474
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-06-01 21:47:42 -0400

    install sanitize-html package

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user')
 const { ignoreFavicon } = require('./middleware')
+const mongoSanitize = require('express-mongo-sanitize');
 
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
@@ -34,8 +35,6 @@ db.once('open', () => {
   console.log('Database connected')
 });
 
-
-
 app.engine('ejs', ejsMate)
 app.set('view engine', "ejs")
 app.set("views", path.join(__dirname, "views"))
@@ -43,6 +42,11 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(
+  mongoSanitize({
+    replaceWith: '_',
+  }),
+);
 
 
 const sessionConfig = {
@@ -63,6 +67,7 @@ app.use(flash())
 app.use(ignoreFavicon);
 app.use(passport.initialize())
 app.use(passport.session())
+
 passport.use(new LocalStrategy(User.authenticate()))
 
 passport.serializeUser(User.serializeUser())
