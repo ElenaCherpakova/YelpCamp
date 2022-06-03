a2ef5f22de23332a0569039708dcbb1c014fac55
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-06-02 21:56:11 -0400

    update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
 
+
 const express = require("express");
 const app = express();
 const path = require('path')
@@ -12,9 +13,13 @@ const flash = require('connect-flash')
 const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const User = require('./models/user')
+const User = require('./models/user');
+const helmet = require('helmet');
+
 const { ignoreFavicon } = require('./middleware')
+
 const mongoSanitize = require('express-mongo-sanitize');
+
 
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
@@ -39,6 +44,7 @@ app.engine('ejs', ejsMate)
 app.set('view engine', "ejs")
 app.set("views", path.join(__dirname, "views"))
 
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 app.use(express.static(path.join(__dirname, 'public')))
@@ -61,15 +67,65 @@ const sessionConfig = {
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
 }
-app.use(session(sessionConfig))
-app.use(flash())
-
-
 
 app.use(ignoreFavicon);
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(helmet({ crossOriginEmbedderPolicy: false }));
 
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://res.cloudinary.com//"
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net/",
+  "https://res.cloudinary.com/elena-cherpakova-yelp-camp/"
+];
+const connectSrcUrls = [
+  "https://*.tiles.mapbox.com",
+  "https://api.mapbox.com",
+  "https://events.mapbox.com",
+  "https://res.cloudinary.com/elena-cherpakova-yelp-camp/"
+];
+const fontSrcUrls = [ "https://res.cloudinary.com/elena-cherpakova-yelp-camp/" ];
+
+app.use(
+  helmet.contentSecurityPolicy({
+      directives : {
+          defaultSrc : [],
+          connectSrc : [ "'self'", ...connectSrcUrls ],
+          scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+          styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+          workerSrc  : [ "'self'", "blob:" ],
+          objectSrc  : [],
+          imgSrc     : [
+              "'self'",
+              "blob:",
+              "data:",
+              "https://res.cloudinary.com/elena-cherpakova-yelp-camp/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+              "https://images.unsplash.com/"
+          ],
+          fontSrc    : [ "'self'", ...fontSrcUrls ],
+          mediaSrc   : [ "https://res.cloudinary.com/elena-cherpakova-yelp-camp/" ],
+          childSrc   : [ "blob:" ]
+      }
+  })
+);
+
+
+app.use(passport.initialize());
+app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()))
 
 passport.serializeUser(User.serializeUser())
@@ -87,10 +143,12 @@ app.use((req, res, next) => {
   next();
 })
 
+
+
 app.use('/', userRoutes)
 app.use('/campgrounds', campgroundRoutes)
 app.use('/campgrounds/:id/reviews', reviewRoutes)
-app.use(ignoreFavicon);
+
 
 app.get('/', (req, res) => {
   res.render('home')
@@ -110,3 +168,4 @@ app.use((err, req, res, next) => {
 app.listen(3000, () => {
   console.log("Listening on PORT 3000")
 }) 
+
